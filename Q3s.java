13ef580fc04740392138a9169aec6bd1bab8d4bb
Aakash Deep <97273990+adeepnov@users.noreply.github.com>
2022-01-30 15:40:51 +0530

    Added file from local machine
    Java file to print hello world

---
+++ Q3s.java
@@ -0,0 +1,9 @@
+//3. Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+
+class Q3s
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello\nAakash");
+	}
+}
